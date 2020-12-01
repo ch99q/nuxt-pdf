2cde41b12f9909388ced850e55b1f1431d99ab6d
Christian Hansen <64793a1a@gmail.com>
2020-12-01 14:44:03 +0100

    Fixed undefined title bug

--- playground/nuxt.config.js
+++ playground/nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  // target: 'static',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
